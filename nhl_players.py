e6d0a19e02108c6e800473440eb87d4b61d3cab8
R3vanchist <110637379+R3vanchist@users.noreply.github.com>
2023-02-21 20:05:42 -0500

    Update and rename Web_scraper.py to nhl_players.py

---
+++ nhl_players.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import requests
+
+# Enter the player's name and team
+player_name = input("Enter player's name: ")
+team_name = input("Enter team name: ")
+
+# Create the URL to get the team's id
+team_url = f"https://statsapi.web.nhl.com/api/v1/teams/"
+team_response = requests.get(team_url)
+team_data = team_response.json()
+team_id = None
+for team in team_data["teams"]:
+    team_names = team["name"]
+    team_ids = team["id"]
+    if team_name in team_names:
+        team_id = team_ids
+
+# Create the URL to get the player's id
+
+player_url = f"https://statsapi.web.nhl.com/api/v1/teams/{team_id}/roster"
+player_response = requests.get(player_url)
+player_data = player_response.json()
+player_id = None
+for players in player_data["roster"]:
+    playerName = players["person"]["fullName"]
+    playerId = players["person"]["id"]
+    if player_name in playerName:
+        player_id = playerId
+
+# Create the URL to get the player's game log
+stats_url = f"https://statsapi.web.nhl.com/api/v1/people/{player_id}/stats?stats=gameLog&season=20222023"
+stats_response = requests.get(stats_url)
+stats_data = stats_response.json()
+
+# Create a pandas dataframe with the player's last 10 games' stats
+stats = stats_data['stats'][0]['splits'][:10
+]
+player_stats = []
+for game in stats:
+    game_stats = {
+        'Shots': game['stat']['shots'],
+        'Goals': game['stat']['goals'],
+        'Assists': game['stat']['assists'],
+        'Points': game['stat']['points'],
+        }
+    player_stats.append(game_stats)
+
+df = pd.DataFrame(player_stats)
+
+# Write the dataframe to an Excel file
+writer = pd.ExcelWriter(f"{player_name}_last_10_games.xlsx")
+df.to_excel(writer, index=False)
+writer.save()
+
+print(f"{player_name}'s last 10 games' stats saved to {player_name}_last_10_games.xlsx")
