a2023a25dcb01b77a755012389b37452a19110b9
R3vanchist <110637379+R3vanchist@users.noreply.github.com>
2023-02-23 16:21:04 +0000

    Update nhl_schedule.py

--- nhl_schedule.py
+++ nhl_schedule.py
@@ -1,29 +1,32 @@
 import requests
 import datetime
 
-# set the date to today
-today = datetime.date.today()
 
-# format the date in the required format for the API (YYYY-MM-DD)
-date_str = today.strftime("%Y-%m-%d")
+def nhl_schedule():
+    today = datetime.date.today()
 
-# make the API request to retrieve the daily schedule
-url = f"https://statsapi.web.nhl.com/api/v1/schedule?date={date_str}&expand=schedule.teams"
-response = requests.get(url)
+    # format the date in the required format for the API (YYYY-MM-DD)
+    date_str = today.strftime("%Y-%m-%d")
 
-# check if the request was successful
-if response.status_code == 200:
-    # parse the JSON response
-    data = response.json()
-    
-    # extract the game information from the response
-    games = data["dates"][0]["games"]
-    
-    # iterate over the games and print the information
-    for game in games:
-        away_team = game["teams"]["away"]["team"]["name"]
-        home_team = game["teams"]["home"]["team"]["name"]
-        game_time = game["gameDate"]
-        print(f"{away_team} @ {home_team} ({game_time})")
-else:
-    print(f"Error retrieving NHL schedule: {response.status_code}")
+    # make the API request to retrieve the daily schedule
+    url = f"https://statsapi.web.nhl.com/api/v1/schedule?date={date_str}&expand=schedule.teams"
+    response = requests.get(url)
+
+    filename = 'todays_games.txt'
+    with open(filename, 'w') as todays_teams:
+        # check if the request was successful
+        if response.status_code == 200:
+            # parse the JSON response
+            data = response.json()
+            
+            # extract the game information from the response
+            games = data["dates"][0]["games"]
+            
+            # iterate over the games and print the information
+            for game in games:
+                away_team = game["teams"]["away"]["team"]["name"]
+                home_team = game["teams"]["home"]["team"]["name"]
+                todays_teams.write(f"{away_team}\n{home_team}\n")
+        else:
+            print(f"Error retrieving NHL schedule: {response.status_code}")
+nhl_schedule()
