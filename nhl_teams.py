31a6617c7c2e9196c4b0ae5ca62df07c5258101a
R3vanchist <110637379+R3vanchist@users.noreply.github.com>
2023-02-23 15:38:44 +0000

    Update nhl_teams.py

--- nhl_teams.py
+++ nhl_teams.py
@@ -1,12 +1,41 @@
-import pandas as pd
+import csv
+import json
 
-# read in the data
-data = pd.read_csv('nhl_data.csv')
+def conv_csv_json():
+    with open('nhl_data.csv', 'r') as csvfile:
+        # Create a CSV reader object
+        csvreader = csv.reader(csvfile)
+        
+        # Get the header row from the CSV file
+        headers = next(csvreader)
+        
+        # Create an empty list to hold the rows of data
+        data = []
+        
+        # Iterate over the remaining rows in the CSV file
+        for row in csvreader:
+            # Create a dictionary to hold the data for this row
+            row_data = {}
+            
+            # Iterate over the columns in the row, and add them to the dictionary
+            for i in range(len(headers)):
+                row_data[headers[i]] = row[i]
+            
+            # Add the dictionary to the list of data
+            data.append(row_data)
 
-# calculate the average goals scored by each team over the last 10 games
-avg_goals = data.groupby('TEAM')['G'].mean()
-avg_goals_against = data.groupby('TEAM')['GA'].mean()
+    # Convert the list of dictionaries to a JSON string
+    json_string = json.dumps(data, indent=4)
 
-# print the results
-print(f"Average goals are: {avg_goals}")
-print(f"Average goals against are: {avg_goals_against}")
+    with open('nhl_data.json', 'w') as jsonfile:
+        jsonfile.write(json_string)
+conv_csv_json()
+def compare_json_txt():
+    with open('todays_games.txt', 'r') as games, open('nhl_data.json', 'r') as stats:
+        txt_data = games.read()
+        json_data = json.load(stats)
+        # Compare the text data to the JSON data
+        for team in json_data:
+            if team['TEAM'] in txt_data:
+                print(team['TEAM'], team['G/G'], team['GA/G'])
+compare_json_txt()
