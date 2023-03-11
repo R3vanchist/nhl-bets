006e9a55dc3f63c7c896ab8c0861b65c9044387e
R3vanchist <110637379+R3vanchist@users.noreply.github.com>
2023-03-11 11:45:13 -0500

    Update nhl_app.py

--- nhl_app.py
+++ nhl_app.py
@@ -1,8 +1,14 @@
 from team_stats import team_stat_pull
 from nhl_schedule import schedule
-from daily_team_stats import daily_stats
+from daily_team_stats import team_scoring_three
+from daily_team_stats import team_not_scoring_three
+from daily_team_stats import team_overs
+from daily_team_stats import team_unders
 
 
 schedule()
 team_stat_pull()
-daily_stats()
+team_scoring_three()
+team_not_scoring_three()
+team_overs()
+team_unders()
